132e69a166ed114392a2a0433262fa95122f2260
Tessa Kelly <tessakkelly@gmail.com>
2016-11-15 20:10:53 -0800

    Adds color palette

---
+++ src/Colors.elm
@@ -0,0 +1,82 @@
+module Colors exposing (..)
+
+import Css exposing (..)
+
+
+blue0 : Css.Color
+blue0 =
+    hex "#343477"
+
+
+blue1 : Css.Color
+blue1 =
+    hex "#CDCDDB"
+
+
+blue2 : Css.Color
+blue2 =
+    hex "#7171A4"
+
+
+blue3 : Css.Color
+blue3 =
+    hex "#0D0D4A"
+
+
+blue4 : Css.Color
+blue4 =
+    hex "#01011C"
+
+
+mustard0 : Css.Color
+mustard0 =
+    hex "#AAA039"
+
+
+mustard1 : Css.Color
+mustard1 =
+    hex "#FFFDED"
+
+
+mustard2 : Css.Color
+mustard2 =
+    hex "#EBE394"
+
+
+mustard3 : Css.Color
+mustard3 =
+    hex "#696004"
+
+
+mustard4 : Css.Color
+mustard4 =
+    hex "#282500"
+
+
+colorSecondary20 : Css.Color
+colorSecondary20 =
+    hex "#AA7939"
+
+
+clay1 : Css.Color
+clay1 =
+    hex "#FFF7ED"
+
+
+clay2 : Css.Color
+clay2 =
+    hex "#EBC694"
+
+
+clay3 : Css.Color
+clay3 =
+    hex "#693E04"
+
+
+clay4 : Css.Color
+clay4 =
+    hex "#281700"
+
+
+
+--Colors from Paletton.com © 2002-2014, http://paletton.com
