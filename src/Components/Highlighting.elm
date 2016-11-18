de0010db709613e1a16abb7b6c0a227c9d89c97b
Tessa Kelly <tessakkelly@gmail.com>
2016-11-17 20:22:40 -0800

    Adds initial version of the highlighting view, for one highlighting color

---
+++ src/Components/Highlighting.elm
@@ -0,0 +1,57 @@
+module Components.Highlighting exposing (Model, update, Msg(..), view)
+
+import Colors
+import Css
+import Html exposing (..)
+import Styles
+import TextUp
+
+
+type alias Model =
+    { fragments : List ( String, Maybe Color )
+    }
+
+
+type Color
+    = Yellow
+
+
+type alias HighlightStyles =
+    { yellow : Css.Mixin
+    }
+
+
+highlightStyles : TextUp.Config HighlightStyles
+highlightStyles =
+    { plain = Styles.preserveWhiteSpace
+    , yellow = Css.mixin [ Css.backgroundColor Colors.neonYellow ]
+    }
+
+
+toTextUpString : ( String, Maybe Color ) -> TextUp.TextUpString HighlightStyles
+toTextUpString ( str, maybeColor ) =
+    (,) str <|
+        case maybeColor of
+            Just Yellow ->
+                .yellow
+
+            Nothing ->
+                .plain
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            model ! []
+
+
+view : Model -> Html Msg
+view model =
+    model.fragments
+        |> List.map toTextUpString
+        |> TextUp.toHtml highlightStyles
