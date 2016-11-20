11e064fb62de91f39a91bbebd7146336784f2557
Tessa Kelly <tessakkelly@gmail.com>
2016-11-20 12:02:21 -0800

    Expand the highlighter color options

--- src/Data/HighlighterColor.elm
+++ src/Data/HighlighterColor.elm
@@ -8,18 +8,35 @@ import Theme.Mixins as Mixins
 
 type Color
     = Yellow
+    | Blue
+    | Green
+    | Pink
+    | Red
 
 
 type alias HighlightStyles =
     { yellow : Css.Mixin
+    , blue : Css.Mixin
+    , green : Css.Mixin
+    , pink : Css.Mixin
+    , red : Css.Mixin
     }
 
 
 highlightStyles : TextUp.Config HighlightStyles
 highlightStyles =
     { plain = Mixins.preserveWhiteSpace
-    , yellow = Css.mixin [ Css.backgroundColor Colors.neonYellow ]
+    , yellow = highlight Colors.neonYellow
+    , blue = highlight Colors.neonBlue
+    , green = highlight Colors.neonGreen
+    , pink = highlight Colors.neonPink
+    , red = highlight Colors.neonRed
     }
+
+
+highlight : Css.ColorValue compatible -> Css.Mixin
+highlight color =
+    Css.mixin [ Css.backgroundColor color ]
 
 
 toTextUpString : ( String, Maybe Color ) -> TextUp.TextUpString HighlightStyles
@@ -29,13 +46,30 @@ toTextUpString ( str, maybeColor ) =
             Just Yellow ->
                 .yellow
 
+            Just Blue ->
+                .blue
+
+            Just Green ->
+                .green
+
+            Just Pink ->
+                .pink
+
+            Just Red ->
+                .red
+
             Nothing ->
                 .plain
 
 
 highlightColors : List Color
 highlightColors =
-    [ Yellow ]
+    [ Yellow
+    , Blue
+    , Green
+    , Pink
+    , Red
+    ]
 
 
 colorToString : Color -> String
@@ -43,3 +77,15 @@ colorToString color =
     case color of
         Yellow ->
             "Yellow"
+
+        Blue ->
+            "Blue"
+
+        Green ->
+            "Green"
+
+        Pink ->
+            "Pink"
+
+        Red ->
+            "Red"
